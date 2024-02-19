2e3565170fbf182ac6df9e690ef24adf349735c5
gregolly <gregollyff@hotmail.com>
2024-02-19 08:15:51 -0400

    feat: add form validation

--- helpers.py
+++ helpers.py
@@ -1,5 +1,14 @@
 import os
 from gamelib import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, SubmitField
+
+class GameForm(FlaskForm):
+    name = StringField('Game name', [validators.data_required(), validators.length(min=1, max=50)])
+    category = StringField('Category', [validators.data_required(), validators.length(min=1, max=40)])
+    console = StringField('Console', [validators.data_required(), validators.length(min=1, max=20)])
+    save = SubmitField('Save')
+    
 
 def recover_image(id):
     for file_name in os.listdir(app.config['UPLOAD_PATH']):
@@ -11,3 +20,5 @@ def delete_file(id):
     file = recover_image(id)
     if file != 'default-cover.jpg':
         os.remove(os.path.join(app.config['UPLOAD_PATH'], file))
+
+
